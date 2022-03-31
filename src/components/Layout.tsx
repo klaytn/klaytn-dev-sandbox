5f4ea6b389729adf90da1ad3627c151f34c3aed7
Chloe Tan <chloe.t@krustuniverse.com>
2022-03-31 16:01:12 +0800

    removed unused code in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,36 +7,18 @@ import providerContext from '../context/context'
 declare const window: any
 
 const Layout = ({ children }: { children: any }) => {
-  const { ethProvider, klaytnProvider, setKlaytnProvider, setEthProvider } =
-    useContext(providerContext)
-
-  const checkKaikasStatus = async () => {
-    const enabled = klaytnProvider._kaikas.isEnabled()
-    const approved = await klaytnProvider._kaikas.isApproved()
-    const unlocked = await klaytnProvider._kaikas.isUnlocked()
-    // console.log('enabled: ', enabled)
-    // console.log('approved: ', approved)
-    // console.log('unlocked: ', unlocked)
-  }
+  const { setKlaytnProvider, setEthProvider } = useContext(providerContext)
 
   useEffect(() => {
     if (typeof window.klaytn !== 'undefined') {
       const provider = window['klaytn']
       setKlaytnProvider(provider)
-      // console.log('klay provider:', provider)
     }
     if (typeof window.ethereum !== 'undefined') {
       const provider = window.ethereum
       setEthProvider(provider)
-      // console.log('eth provider:', provider)
     }
   }, [])
-
-  useEffect(() => {
-    if (klaytnProvider) {
-      checkKaikasStatus()
-    }
-  }, [klaytnProvider])
 
   return (
     <div className="flex flex-col h-screen justify-between font-Outfit">
