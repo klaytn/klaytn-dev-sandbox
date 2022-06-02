1a147a78d7ed5aac31e6318d6d32ed008a438aa6
tantely-klaytn <tantely.a@krustuniverse.com>
2022-06-02 15:33:21 +0200

    replace imports and update functions

--- contracts/token/KIP17/KIP17Token.sol
+++ contracts/token/KIP17/KIP17Token.sol
@@ -2,29 +2,34 @@
 
 pragma solidity ^0.8.0;
 
-import './extensions/KIP17Metadata.sol';
-import '../../utils/Ownable.sol';
-import '../../utils/Counters.sol';
+//import './extensions/KIP17Metadata.sol';
+import "@klaytn/contracts/contracts/KIP/token/KIP17/KIP17.sol";
+import "@klaytn/contracts/contracts/KIP/token/KIP17/extensions/KIP17MetadataMintable.sol";
+import "@klaytn/contracts/contracts/access/Ownable.sol";
+import "@klaytn/contracts/contracts/utils/Counters.sol";
+//import '../../utils/Ownable.sol';
+//import '../../utils/Counters.sol';
 
 
-contract KIP17Token is KIP17Metadata, Ownable {
+contract KIP17Token {
     
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    // using Counters for Counters.Counter;
+    // Counters.Counter private _tokenIds;
 
-    constructor(string memory name, string memory symbol) KIP17Metadata(name, symbol) {}
+    // constructor(string memory name, string memory symbol) KIP17MetadataMintable(name, symbol) {}
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
+    // function mintNFT(address recipient, string memory tokenURI)
+    //     public onlyOwner
+    //     returns (uint256)
+    // {
+    //     _tokenIds.increment();
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    //     uint256 newItemId = _tokenIds.current();
+    //    // mintWithTokenURI(recipient, newItemId, tokenURI);
+    //     _mint(recipient, newItemId);
+    //     _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
-    }
+    //     return newItemId;
+    // }
 }
 
